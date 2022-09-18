0b4abd26c5d0ab72b1cfa92772ed2024cccb1cfb
boschoda <boschoda@mail.uc.edu>
2022-09-18 17:14:17 -0400

    lab3 files

---
+++ powershell/lab3boschovich.ps1
@@ -0,0 +1,22 @@
+﻿function getIP{
+    (Get-NetIPAddress).IPv4Address | Select-String "192*"
+    }
+function getDate{
+    (Get-Date).Date
+    }
+
+function getVersion{
+    (Get-Host).Version
+    }
+function getUser{
+    $env:USERNAME
+    }
+
+$IP = getIP
+$USER = getUser
+$DATE = getDate
+$VERSION = getVersion
+
+$BODY = "This machine's IP is $IP. User is $USER. The version is POWERSHELL $VERSION. Today's date is $DATE"
+
+Write-Host($BODY)
